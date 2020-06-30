f8c90b7dd3ecfa36f2c72635935dc03ac785d33e
AliDorri <dorri@unboundgamestudio.com>
2020-06-30 23:27:34 +0430

    added user id list to Publication

--- PublicationSystem/PublicationSystem/Publication.cs
+++ PublicationSystem/PublicationSystem/Publication.cs
@@ -11,11 +11,13 @@ namespace PublicationSystem
 {
     public class Publication : ICommandExecuter
     {
+        private PublicationState m_State = new Editing();
+        private List<int> m_SubscriberIDs = new List<int>();
+
         public string Name { get; set; }
         public DateTime EstablishDate { get; set; }
         public int RegisterationNumber { get; set; }
         public string HeadName { get; set; }
-        private PublicationState m_State = new Editing();
 
         public override string ToString()
         {
@@ -34,6 +36,25 @@ namespace PublicationSystem
         public bool Execute(string command, string[] args)
         {
             return m_State.Execute(command, args, this);
+        }
+
+        public void Subscribe(int userID)
+        {
+            bool subsribedBefore = m_SubscriberIDs.Contains(userID);
+            if (!subsribedBefore)
+            {
+                m_SubscriberIDs.Add(userID);
+            }
+        }
+
+        public void Unsubcribe(int userID)
+        {
+            m_SubscriberIDs.Remove(userID);
+        }
+
+        public int GetUserID(int index)
+        {
+            return m_SubscriberIDs[index];
         }
     }
 }
