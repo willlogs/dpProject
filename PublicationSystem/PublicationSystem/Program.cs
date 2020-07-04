b599ccb098921ddeacaef9968a0032970fc71027
MH <mre.ezboy3@gmail.com>
2020-07-04 22:07:51 +0430

    subscribtion added

--- PublicationSystem/PublicationSystem/Program.cs
+++ PublicationSystem/PublicationSystem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using PublicationSystem.CommandProcessing;
+using PublicationSystem.Info;
 using PublicationSystem.StylishPrinting;
 using System;
 using System.Collections.Generic;
@@ -12,16 +13,20 @@ namespace PublicationSystem
     {
         static void Main(string[] args)
         {
-            CommandProcessor.Instance.Subscribe(PublicationCommandExecuter.Instance);
-            CommandProcessor.Instance.Subscribe(new PrintingCommandExecuter());
-            StylishPrinter.AddStyle(new StarStyle());   
-            StylishPrinter.AddStyle(new QuestionStyle());   
-            StylishPrinter.AddStyle(new UpperCaseStyle());   
+            CommandProcessor cp = CommandProcessor.Instance;
+
+            cp.Subscribe(UserManager.Instance);
+            cp.Subscribe(PublicationCommandExecuter.Instance);
+            cp.Subscribe(new PrintingCommandExecuter());
+
+            StylishPrinter.AddStyle(new StarStyle());
+            StylishPrinter.AddStyle(new QuestionStyle());
+            StylishPrinter.AddStyle(new UpperCaseStyle());
 
             while (true)
             {
                 string command = Console.ReadLine();
-                CommandProcessor.Instance.ParseCommand(command);
+                cp.ParseCommand(command);
             }
         }
     }
